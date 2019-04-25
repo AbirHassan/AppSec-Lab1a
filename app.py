bd4864d25a0fbeeb66673551edca13e82caba05e
Abir Hassan <ash521@nyu.edu>
2019-04-24 20:12:22 -0400

    changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 from PIL import Image
 from flask import Flask, render_template, flash, request, redirect, url_for
@@ -14,12 +15,12 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def make_square(im, thumbnail_size):
-        background = Image.new('RGB', thumbnail_size, "black")    
-        source_image = im.convert("RGB")
-        source_image.thumbnail(thumbnail_size)
-        (w, h) = source_image.size
-        background.paste(source_image, ((thumbnail_size[0] - w) // 2, (thumbnail_size[1] - h) // 2 ))
-        return background
+    background = Image.new('RGB', thumbnail_size, "black")
+    source_image = im.convert("RGB")
+    source_image.thumbnail(thumbnail_size)
+    (w, h) = source_image.size
+    background.paste(source_image, ((thumbnail_size[0] - w) // 2, (thumbnail_size[1] - h) // 2))
+    return background
 
 @app.route('/')
 @app.route('/index')
@@ -35,15 +36,15 @@ def resize():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(url_for("index"))
-        f = request.files['file']
+        reqfile = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
-        if f.filename == '':
+        if reqfile.filename == '':
             flash('No selected file')
             return redirect(url_for("index"))
-        if f and allowed_file(f.filename):
-            filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if reqfile and allowed_file(reqfile.filename):
+            filename = secure_filename(reqfile.filename)
+            reqfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
             #source: https://stackoverflow.com/questions/44231209/resize-rectangular-image-to-square-keeping-ratio-and-fill-background-with-black/44231784
             file_dimension = request.form['size']
@@ -53,7 +54,6 @@ def resize():
             test_image = Image.open("images/" + filename)
             img = make_square(test_image, (int(file_dimension), int(file_dimension)))
             img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            
             return redirect(url_for("index"))
         else:
             flash('Incorrect file')
